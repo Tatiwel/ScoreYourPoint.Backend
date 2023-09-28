fdd8a47de0f8e00cbe0b083a69864ea19f27fd6d
Lucas Litieri Brentz Andrade Costa <53198676+GoldMember33@users.noreply.github.com>
2023-09-27 21:02:43 -0300

    Added more Controller classes

---
+++ ScoreYourPoint.Api/Controllers/ProfileController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScoreYourPoint.Dto;
+using ScoreYourPointAPI.Domain;
+using ScoreYourPointApi.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using ScoreYourPointApi.Domain;
+
+namespace ScoreYourPoint.Api.Controllers
+{
+
+    public class ProfileController : Controller
+    {
+
+        private readonly DataContext _dataContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        // GET: ProfileController
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProfileDto>> GetByID(int id)
+        {
+            var Profile = await _dataContext.Profiles.FirstOrDefaultAsync(pro => pro.Id == id);
+
+            if (Profile == null)
+            {
+                return NotFound();
+            }
+
+            return new ProfileDto(Profile);
+        }
+
+        // POST: ProfileController
+        [HttpPost]
+        public async Task<ActionResult> Store([FromBody] ProfileDto profile)
+        {
+            await _dataContext.Profiles.AddAsync(new Profile
+            {
+                Id = profile.Id,
+                Name = profile.Name,
+                Gender = profile.Gender,
+                Age = profile.Age,
+                LeftOrRight = profile.LeftOrRight,
+                Height = profile.Height,
+                Weight = profile.Weight,
+            });
+
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PUT: ProfileController
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] ProfileDto profile)
+        {
+            var Prof = await _dataContext.Profiles.FirstOrDefaultAsync(prof => prof.Id == id);
+
+            if (Prof == null)
+            {
+                return NotFound();
+            }
+
+            Prof.Id = profile.Id;
+            Prof.Name = profile.Name;
+            Prof.Gender = profile.Gender;
+            Prof.Age = profile.Age;
+            Prof.LeftOrRight = profile.LeftOrRight;
+            Prof.Height = profile.Height;
+            Prof.Weight = profile.Weight;
+
+            _dataContext.Entry(Prof).CurrentValues.SetValues(profile);
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: ProfileController
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var Prof = await _dataContext.Profiles.FirstOrDefaultAsync(prof => prof.Id == id);
+
+            if (Prof == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(Prof);
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ScoreYourPoint.Api/Controllers/SportController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScoreYourPoint.Dto;
+using ScoreYourPointApi.Infra.Data;
+using ScoreYourPointAPI.Domain;
+
+namespace ScoreYourPoint.Api.Controllers
+{
+    public class SportController : Controller
+    {
+        // GET: SportController
+        private readonly DataContext _dataContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SportDto>> GetByID(int id)
+        {
+            var Sport = await _dataContext.Sports.FirstOrDefaultAsync(sp => sp.Id == id);
+
+            if (Sport == null)
+            {
+                return NotFound();
+            }
+
+            return new SportDto(Sport);
+        }
+
+        // POST: SportController
+        [HttpPost]
+        public async Task<ActionResult> Store([FromBody] SportDto sport)
+        {
+            await _dataContext.Sports.AddAsync(new Sport
+            {
+                Id = sport.Id,
+                Name = sport.Name,
+            });
+
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PUT: SportController
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] SportDto sport)
+        {
+            var Spt = await _dataContext.Sports.FirstOrDefaultAsync(sp => sp.Id == id);
+
+            if (Spt == null)
+            {
+                return NotFound();
+            }
+
+            Spt.Id = sport.Id;
+            Spt.Name = sport.Name;
+
+            _dataContext.Entry(Spt).CurrentValues.SetValues(sport);
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: SportController
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var Spt = await _dataContext.Sports.FirstOrDefaultAsync(sp => sp.Id == id);
+
+            if (Spt == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(Spt);
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
